0d35b4f10cf97574e045ed0088eda6221ad804d1
Vinicius Marins <vmarins.dev@gmail.com>
2022-03-10 19:26:49 -0300

    Initial commit

---
+++ src/AppPessoa.java
@@ -0,0 +1,19 @@
+public class AppPessoa {
+
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa("Rafael","rafael@rafael.com",2500);
+
+        p1.exibirDados();
+        p1.aumentarSalario(20);
+        p1.exibirDados();
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,52 @@
+public class Pessoa {
+
+    private String nome;
+    private String email;
+    private float salario;
+
+
+    public Pessoa(String nome, String email, float salario) {
+        this.nome = nome;
+        this.email = email;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public void aumentarSalario(float indice){
+        salario = salario + salario*indice/100;
+    }
+
+    public void exibirDados(){
+        System.out.println("Sistema de Pessoas ********");
+        System.out.println("Nome: "+ nome);
+        System.out.println("E-mail: " + email);
+        System.out.println("Salario: R$ " + salario);
+    }
+
+
+}
